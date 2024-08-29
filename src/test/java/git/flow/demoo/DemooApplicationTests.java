1343ff7dd5ff86d6b61a44084d209f9863cd5681
trungkientlu495 <nguyentrungktlu495@gmail.com>
2024-08-29 22:07:35 +0700

    demo git flow

---
+++ src/test/java/git/flow/demoo/DemooApplicationTests.java
@@ -0,0 +1,13 @@
+package git.flow.demoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
