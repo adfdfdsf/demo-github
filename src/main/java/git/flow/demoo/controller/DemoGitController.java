a65468016bcd0a88461675227651b19679752a6a
trungkientlu495 <nguyentrungktlu495@gmail.com>
2024-08-29 22:23:41 +0700

    update demo git flow

---
+++ src/main/java/git/flow/demoo/controller/DemoGitController.java
@@ -0,0 +1,10 @@
+package git.flow.demoo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/git")
+public class DemoGitController {
+
+}
